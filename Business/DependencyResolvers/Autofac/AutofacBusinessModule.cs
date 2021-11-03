c169420c329dfd19c41d3ac42046b43b7c6142a3
zkozak34 <zeynel.kozak@outlook.com>
2021-11-04 01:28:24 +0300

    Added Autofac IoC Container

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,17 @@
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<ProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
